236126228b43d91f0e980a77b17cbd2bf51c4c2b
Lainey Creighton <dev.lainey@gmail.com>
2023-08-10 12:32:59 -0400

    add database access and defined connection settings

--- models/Post.js
+++ models/Post.js
@@ -1,9 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class Post extends Model {}
 
-User.init(
+Post.init(
   {
     // Define columns
     id: {
@@ -29,12 +29,6 @@ User.init(
     },
   },
   {
-    hooks: {
-        beforeCreate: async (newUserData) => {
-          newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
-        },
-      },
     sequelize,
     timestamps: false,
     freezeTableName: true,
