ce16849f0376c8d1c520cfb64d7232102929a3f6
Lainey Creighton <dev.lainey@gmail.com>
2023-08-11 23:04:54 -0400

    add controllers and routes for user login and registration

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const userController = require('../../controllers/userController');
+
+// Route for user registration
+router.post('/register', userController.register);
+
+// Route for user login
+router.post('/login', userController.login);
+
+module.exports = router;
