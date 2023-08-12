84168ffde5bb44afaa8a9f2ab67dcea49b383265
Lainey Creighton <dev.lainey@gmail.com>
2023-08-11 23:16:21 -0400

    add update to connection

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,21 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: process.env.DB_HOST,
-            dialect: 'mysql',
-            port: 3306,
-        }
-    );
+let sequelize;
 
-    module.exports = sequelize;
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+}
+
+module.exports = sequelize;
