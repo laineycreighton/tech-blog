1a147f267973f629fdeb2e7f90941c0f0e4b594d
Lainey Creighton <dev.lainey@gmail.com>
2023-08-11 23:13:59 -0400

    add jaws to connection

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
-// config.js
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: 3306,
-  }
-);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'mysql',
+            port: 3306,
+        }
+    );
 
-module.exports = sequelize;
+    module.exports = sequelize;
