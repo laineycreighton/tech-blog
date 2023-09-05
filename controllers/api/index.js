461981ee63af3851b6abd52be0861a4596000abd
Lainey <dev.lainey@gmail.com>
2023-09-05 14:14:22 -0400

    add routes to api index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const postRoutes = require('./postRoutes');
+const commentRoutes = require('./commentRoutes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
