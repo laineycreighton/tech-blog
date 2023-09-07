e3c778dff36cb782263132b05b7c7ea35def7557
Lainey <dev.lainey@gmail.com>
2023-09-06 21:53:31 -0400

    change connection

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,13 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USER, 
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+});
 
 module.exports = sequelize;
