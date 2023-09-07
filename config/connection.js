52f0d0b4b4b3479a7f1d7d64de5cbc21e89a1a52
Lainey <dev.lainey@gmail.com>
2023-09-06 22:13:36 -0400

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  host: '127.0.0.1',
+  port: '3306',
   username: process.env.DB_USER, 
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME, 
